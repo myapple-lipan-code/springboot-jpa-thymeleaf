a28d71b45236ad320c5e4e96df483d8e630e5e63
sunny <lipan1587954@163.com>
2020-01-08 19:07:58 +0800

    新增注释

--- src/main/java/com/ujiuye/SpringbootJpaThymeleafApplication.java
+++ src/main/java/com/ujiuye/SpringbootJpaThymeleafApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootJpaThymeleafApplication {
-
+    /**
+     * 新增注释*****************************
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJpaThymeleafApplication.class, args);
     }
